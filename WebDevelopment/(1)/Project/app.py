e462a22f1a5e92e7eb1d3cc90084aa516304212a
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-02-16 19:54:10 +0900

    웹개발

---
+++ WebDevelopment/(1)/Project/app.py
@@ -0,0 +1,11 @@
+# 필수 라이브러리
+'''
+0. Flask : 웹서버를 시작할 수 있는 기능. app이라는 이름으로 플라스크를 시작한다
+1. render_template : html파일을 가져와서 보여준다
+'''
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
