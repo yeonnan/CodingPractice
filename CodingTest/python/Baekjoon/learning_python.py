9ac73bfb4681509087c4d05db712326454127dcb
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-03-02 21:12:01 +0900

    python 배우기 1~50 - 5번까지

---
+++ CodingTest/python/Baekjoon/learning_python.py
@@ -0,0 +1,18 @@
+# 1. 2557 Hello World
+print('Hello World!')
+
+# 2. 1000	A+B
+A, B = input().split()
+print(int(A) + int(B))
+
+# 3. 10998	A×B
+A, B = input().split()
+print(int(A) * int(B))
+
+# 4. 1001	A-B
+A, B = input().split()
+print(int(A) - int(B))
+
+# 5. 1008	A/B
+A, B = input().split()
+print(int(A) / int(B))
